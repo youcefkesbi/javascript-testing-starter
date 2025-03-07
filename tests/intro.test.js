8016479172c9fc0b30ad3365b1c841bcaee23888
Youcef Kesbi <ykesbi@gmail.com>
2025-03-07 07:38:07 +0300

    update

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,1 +1,16 @@
 import { describe, test, it, expect } from "vitest";
+import { sum, max } from "../src/intro";
+
+describe('max', () => {
+    it('should return the first argument if it is greater', () => {
+        expect(max(2, 1)).toBe(2);
+    })
+
+    it('should return the second argument if it is greater', () => {
+        expect(max(1, 2)).toBe(2);
+    })
+
+    it('should return the first argument if the arguments are equal', () => {
+        expect(max(1, 1)).toBe(1);
+    })
+})

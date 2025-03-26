c7b8e1a53664c72cade89f124d5a58955e9ccb4b
Youcef Kesbi <ykesbi@gmail.com>
2025-03-26 10:46:40 +0300

    FizzBuzz test

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { sum, max } from "../src/intro";
+import { sum, max, fizzBuzz } from "../src/intro";
 
 describe('max', () => {
     it('should return the first argument if it is greater', () => {
@@ -12,5 +12,17 @@ describe('max', () => {
 
     it('should return the first argument if the arguments are equal', () => {
         expect(max(1, 1)).toBe(1);
+    })
+})
+
+describe('fizzBuzz', () => {
+    it('shoulld return FizzBuzz if arg is divisible by 3 and 5', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+    it('shoulld return Fizz if arg is only divisible by 3', () => {
+        expect(fizzBuzz(9)).toBe('Fizz');
+    })
+    it('shoulld return Buzz if arg is only divisible by 5', () => {
+        expect(fizzBuzz(10)).toBe('Buzz');
     })
 })
